13b581bad2f6bf49a085d66bc6087ac8b55163dc
Victor Felder <victorfelder@gmail.com>
2014-10-22 13:24:08 -0400

    backend controllers

--- server/controllers/presentation.js
+++ server/controllers/presentation.js
@@ -4,15 +4,22 @@ var User = require('../models/user');
 
 
 module.exports.addPresentation = function(req, res) {
+  var presentation = new Presentation(req.body);
+  presentation.save();
   res.send({'result': 'ok'});
 };
 
 
 module.exports.cancelPresentation = function(req, res) {
-  res.send({'result': 'ok'});
+  toCancel = req.body.id;
+  Presentation.findOneAndRemove(function(err, deleted) {
+    res.send({'result': 'ok'});
+  });
 };
 
 
 module.exports.listPresentation = function(req, res) {
-  res.send({'result': 'ok'});
+  Presentation.find({}).exec(function(err, presentations) {
+    res.json(presentations);
+  });
 };
