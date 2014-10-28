00223669870b097c7dc780f08574ecc7bd16947a
Steph Samson <sdvsamson@gmail.com>
2014-10-28 11:53:22 -0400

    No auth for list

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.get('/user/me', authController.ensureAuthenticated, userController.getUserData);
 router.post('/add', authController.ensureAuthenticated, presentationController.addPresentation);
 router.post('/cancel', authController.ensureAuthenticated, presentationController.cancelPresentation);
-router.get('/list', authController.ensureAuthenticated, presentationController.listPresentation);
+router.get('/list', presentationController.listPresentation);
 router.get('/presenting', authController.ensureAuthenticated, presentationController.presenting);
 router.get('/totaltime', presentationController.totalTime);
 
